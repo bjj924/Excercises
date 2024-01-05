6a9b14a97147c92d2b1651cea70ae708f193e8b0
Braian Joho <joho.braian@gmail.com>
2024-01-05 17:05:16 -0300

    Dapper Practices

--- Practices/DBConnection/Program.cs
+++ Practices/DBConnection/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Data.SqlClient;
 using System.Configuration;
+using Dapper;
 
 Console.WriteLine("Connection to a DB");
 Console.WriteLine("");
@@ -46,40 +47,71 @@ Console.WriteLine("Create Connection To DB EFCode_DB");
 //using (SqlConnection connection = new SqlConnection(ConfigurationSettings.AppSettings["dbConnection"]))
 
 //Second Form
-using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["dbConnection"].ConnectionString))
-{
-    Console.WriteLine("Opening Connection with the Connection String");
-    connection.Open();
-    Console.WriteLine(connection.State);
+//using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["dbConnection"].ConnectionString))
+//{
+//    Console.WriteLine("Opening Connection with the Connection String");
+//    connection.Open();
+//    Console.WriteLine(connection.State);
 
-    Console.WriteLine("Defining Commands and parameters for a query");
-    var cmd = new SqlCommand("select * from Users where Id= @id", connection);
-    cmd.Parameters.Add(new SqlParameter("Id", id));
+//    Console.WriteLine("Defining Commands and parameters for a query");
+//    var cmd = new SqlCommand("select * from Users where Id= @id", connection);
+//    cmd.Parameters.Add(new SqlParameter("Id", id));
 
-    Console.WriteLine("Executing the Query");
-    using (SqlDataReader reader= cmd.ExecuteReader())
-    {
-        Console.WriteLine("Mapping to the object User");
-        while (reader.Read())
-        { 
-            user.Id = (int)reader["Id"];
-            user.Name = (string)reader["Name"];
-            user.ContactNumber = (string)reader["ContactNumber"];
+//    Console.WriteLine("Executing the Query");
+//    using (SqlDataReader reader= cmd.ExecuteReader())
+//    {
+//        Console.WriteLine("Mapping to the object User");
+//        while (reader.Read())
+//        { 
+//            user.Id = (int)reader["Id"];
+//            user.Name = (string)reader["Name"];
+//            user.ContactNumber = (string)reader["ContactNumber"];
 
-        }    
-    }
-    Console.WriteLine("Close Connection with the Using");
-}
-Console.WriteLine("");
-Console.WriteLine("Results:");
-Console.WriteLine($"    Id = {user.Id}");
-Console.WriteLine($"    Name = {user.Name}");
-Console.WriteLine($"    Contact Number = {user.ContactNumber}");
-Console.WriteLine("");
+//        }    
+//    }
+//    Console.WriteLine("Close Connection with the Using");
+//}
+//Console.WriteLine("");
+//Console.WriteLine("Results:");
+//Console.WriteLine($"    Id = {user.Id}");
+//Console.WriteLine($"    Name = {user.Name}");
+//Console.WriteLine($"    Contact Number = {user.ContactNumber}");
+//Console.WriteLine("");
 Console.WriteLine("");
 
 Console.WriteLine("-----------------------------------");
+Console.WriteLine("");
+Console.WriteLine("");
 Console.WriteLine("Using Dapper");
+Console.WriteLine("");
+Console.WriteLine("To work with Dapper we need two NuGet Packages");
+Console.WriteLine("The Dapper nuget package by Sam Saffron, Marc Gravell and Nick Craver ");
+Console.WriteLine("(We will use the version 2.1.28)");
+Console.WriteLine("Then the nuget package for Microsoft.Data.SqlClient");
+
+var connectionString = ConfigurationManager.ConnectionStrings["dbConnection"].ConnectionString;
+
+// Ejemplo 1 de Dapper: Query a la tabla
+var query = "select * from Users";
+
+//using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["dbConnection"].ConnectionString))
+//{
+//    //var anonimusQuery = connection.Query(query).ToList();
+
+//    // Podemos mapear el query al objecto que queramos y despues lo hacemos una lista
+//    var users = connection.Query<Users>(query).ToList();
+//}
+
+//Insert con Dapper
+
+var insertQuery = "INSERT INTO Users (Name, ContactNumber) VALUES (@name, @contactnumber)";
+
+using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["dbConnection"].ConnectionString))
+{
+    connection.Execute(insertQuery, new { name = "Prueba", contactnumber ="1134280099" });
+    var users = connection.Query<Users>(query).ToList();
+}
+
 Console.WriteLine("-----------------------------------");
 Console.WriteLine("Using EF");
 Console.WriteLine("-----------------------------------");
